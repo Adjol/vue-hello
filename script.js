f875adcc4f4ce6985f9ad92350f8ac2de71343bb
Adjol <ad.mitrushi@gmail.com>
2023-12-07 17:09:34 +0100

    add image

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ let { createApp } = Vue
 createApp ({
     data(){
         return {
-            message: "Hello world...with Vue"
+            message: "Hello world...with Vue",
+            hWorld__image:("img/HelloWorld.svg.png"),
+            myCssClass: 
         }
-    },
-
+    }
 
 }).mount("#app")
 
